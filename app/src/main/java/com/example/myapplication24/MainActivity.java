7253452491909c634d1adf08030615712ad70782
Milind241 <milinddadore123@gmail.com>
2023-10-22 13:56:12 +0530

    intro page

--- app/src/main/java/com/example/myapplication24/MainActivity.java
+++ app/src/main/java/com/example/myapplication24/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
